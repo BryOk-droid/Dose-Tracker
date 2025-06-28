efe512d3be02946aba9d28af4796419f85fad9e6
BryOk-droid <brianokoth534@gmail.com>
2025-06-29 01:31:42 +0300

    added backend link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: "https://dose-jg6q.onrender.com/patients",
   headers: { "Content-Type": "application/json" },
 });
 
